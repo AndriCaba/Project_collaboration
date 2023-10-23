1039ec93d2b9f776906e0016ab16c7c13e5bc727
Villainoux <villanuevaryan2317@gmail.com>
2023-10-24 01:22:22 +0800

    Updated Player Movement Script (Oct. 24, 2023, - Tuesday - 1:22AM)

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -35,8 +35,7 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxis("Vertical");
 
             Vector3 movementDirection = new Vector3(horizontalInput, 0, verticalInput);
-            float magnitude = (movementDirection.magnitude) * speed;
-            Mathf.Clamp01(magnitude);
+            float magnitude = Mathf.Clamp01(movementDirection.magnitude) * speed;
             movementDirection.Normalize();
                 
                 characterController.SimpleMove(movementDirection * magnitude);
