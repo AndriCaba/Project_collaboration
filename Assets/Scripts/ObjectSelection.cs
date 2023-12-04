dd4c6172e21e377713b0432e6b3e1c16d917ddab
AndriCaba <owens5289@gmail.com>
2023-12-04 20:43:11 +0800

    Organize Hierarchy

--- Assets/Scripts/ObjectSelection.cs
+++ Assets/Scripts/ObjectSelection.cs
@@ -4,8 +4,6 @@ using UnityEngine;
 
 public class ObjectSelection : MonoBehaviour
 {
-
-
     public int[] ID; // Array to store multiple IDs
     private int currentIdIndex = 0;
     public int objectID;
@@ -15,21 +13,34 @@ public class ObjectSelection : MonoBehaviour
         // Check if the collider that hit the enemy has a specific tag
         if (collision.gameObject.CompareTag("Player"))
         {
-            // Cycle to the next ID
-            CycleToNextId();
+            // Check if the ID array is not null and has elements
+            if (ID != null && ID.Length > 0)
+            {
+                // Cycle to the next ID
+                CycleToNextId();
+            }
+            else
+            {
+                Debug.LogError("ID array is null or empty.");
+            }
         }
     }
 
     void CycleToNextId()
     {
-        // Increment the index to move to the next ID
-        currentIdIndex = (currentIdIndex + 1) % ID.Length;
+        // Check if the ID array is not null and has elements
+        if (ID != null && ID.Length > 0)
+        {
+            // Increment the index to move to the next ID
+            currentIdIndex = (currentIdIndex + 1) % ID.Length;
 
-        // Access and use the current ID (replace this with your logic)
-         objectID = ID[currentIdIndex];
-        Debug.Log("Selected ID: " + objectID);
+            // Access and use the current ID (replace this with your logic)
+            objectID = ID[currentIdIndex];
+            Debug.Log("Selected ID: " + objectID);
+        }
+        else
+        {
+            Debug.LogError("ID array is null or empty.");
+        }
     }
-
 }
-
-
